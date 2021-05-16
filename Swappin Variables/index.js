5b11022b51aa9c3fb51d5cc27f771bcdeddc5202
colonelpopcorn2 <maoyanyanr825@163.com>
2021-05-17 05:38:36 +0800

    a

--- Swappin Variables/index.js
+++ Swappin Variables/index.js
@@ -4,10 +4,7 @@ let firstName = "Ocksanna";
 let lastName = "John";
 
 //new code goes here
-define swap(firstName, lastName)
-    temp := firstName
-    firstName := lastName
-    lastName := temp
+
 //print variables to consol
 console.log(firstName);
 console.log(lastName);
