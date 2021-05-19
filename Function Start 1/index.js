de66545e84c5809090eaad12981b377a1db4a6bf
colonelpopcorn2 <maoyanyanr825@163.com>
2021-05-20 05:37:36 +0800

    update

--- Function Start 1/index.js
+++ Function Start 1/index.js
@@ -1,12 +1,14 @@
+//Your function goes here
+
 let OcksannaJohn = {};
 let JaneDoe = {};
 
 OcksannaJohn = ["Ocksanna", "John", 16, 13, false, true]
 JaneDoe = ["Jane", "Doe", 16, 5, false, true]
 
-//printing out a entire object
-console.log(OcksannaJohn);
-console.log(JaneDoe);
+//Your code goes down here
+
+
 
 console.log("Ocksanna's Last Name is" + OcksannaJohn [1]);
 console.log("Jane's First name is:" + JaneDoe [1]);
