5374d204ac41e3527c927bf3d065b02b4aa41cd4
colonelpopcorn2 <maoyanyanr825@163.com>
2021-05-21 15:31:36 +0800

    update x301

--- Function Start 1/index.js
+++ Function Start 1/index.js
@@ -1,16 +1,31 @@
+// Hewwo world function
+function greet (firstName, lastName){
+  console.log('Hewwo' + firstName + "World" + lastName);
+}
+
+
+function square (number){
+return number*number;
+
+
+}
+
+
+
+
 //Your function goes here
 
 let OcksannaJohn = {};
 let JaneDoe = {};
 
-OcksannaJohn = ["Ocksanna", "John", 16, 13, false, true]
+OcksannaJohn = ["Ocksanna", "John", 16, 13, false, true];
 JaneDoe = ["Jane", "Doe", 16, 5, false, true]
+
 
 //Your code goes down here
 
-
-
-console.log("Ocksanna's Last Name is" + OcksannaJohn [1]);
-console.log("Jane's First name is:" + JaneDoe [1]);
-
+//calling functions
+greet (OcksannaJohn [0], OcksannaJohn [1])
+greet (JaneDoe[0], JaneDoe[1])
+greet(square(JaneDoe[2], OcksannaJohn[2]))
 //arrays
