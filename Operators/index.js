c4159393544605caa99f0dc95eda632a8c4f98a8
colonelpopcorn2 <maoyanyanr825@163.com>
2021-05-24 23:57:36 +0800

    woop woop woop

---
+++ Operators/index.js
@@ -0,0 +1,43 @@
+// Hewwo world function
+function greet (firstName, lastName){
+  console.log('Hewwo' + firstName + "World" + lastName);
+}
+
+
+function square (number){
+return number*number;
+
+}
+
+function rewriteLastNameJohn (lastName1, lastName2){
+return OcksannaJohn[1] = lastName2
+
+function rewriteLastNameDoe (lastName1, lastName2){
+return JaneDoe[1] = lastName1
+
+}
+
+
+
+
+//Your function goes here
+
+let OcksannaJohn = {};
+let JaneDoe = {};
+
+OcksannaJohn = ["Ocksanna", "John", 16, 13, false, true];
+JaneDoe = ["Jane", "Doe", 16, 5, false, true]
+
+
+//Your code goes down here
+
+//calling functions
+greet (OcksannaJohn [0], OcksannaJohn [1])
+greet (JaneDoe[0], JaneDoe[1])
+greet(square(JaneDoe[2], OcksannaJohn[2]))
+rewriteLastNameDoe (JaneDoe [1], OcksannaJohn [1])
+rewriteLastNameJohn (OcksannaJohn [1], JaneDoe [1])
+//arrays
+
+console.log ("Jane has the last name of" + JaneDoe[1]);
+console.log ("Ocksanna has the last name of" + OcksannaJohn[1]);
